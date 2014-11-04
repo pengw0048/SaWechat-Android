8ccf0d451d9a32f517dffc04aacd1929f270cbf6
伪红学家 <wang-p12@mails.tsinghua.edu.cn>
2014-11-03 18:48:17 -0800

    Copied GUI from SaWechat-iOS.

---
+++ SaWechat/Form1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Security.Cryptography
+Imports System.Data.SQLite
+
+Public Class Form1
+
+    Private Sub RadioButton1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton1.CheckedChanged
+        GroupBox2.Enabled = True
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton2.CheckedChanged
+        GroupBox2.Enabled = True
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RadioButton3.CheckedChanged
+        GroupBox2.Enabled = False
+    End Sub
+
+End Class
